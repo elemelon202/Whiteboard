381d178307179da583179b2607087c706d3094c7
S Bulman <sm.bulman@gmail.com>
2024-09-01 09:50:20 +0900

    Ready to deploy

---
+++ client/whiteboard/src/App.tsx
@@ -0,0 +1,20 @@
+import { Container, Stack } from "@chakra-ui/react";
+import Navbar from "./Components/Navbar";
+import TodoForm from "./Components/TodoForm";
+import TodoList from "./Components/TodoList";
+
+export const BASE_URL =
+  import.meta.env.MODE === "development" ? "http://localhost:5100/api" : "/api";
+function App() {
+  return (
+    <Stack h="100vh">
+      <Navbar />
+      <Container>
+        <TodoForm />
+        <TodoList />
+      </Container>
+    </Stack>
+  );
+}
+
+export default App;
